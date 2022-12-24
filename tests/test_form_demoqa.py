4d0b2b2716c31e2cdac67b033c2c7448d9efbd0f
Vados61 <pvild3d@gmail.com>
2022-12-24 16:41:49 +0300

    refactor test on models

--- tests/test_form_demoqa.py
+++ tests/test_form_demoqa.py
@@ -1,83 +1,61 @@
-import os
+import allure
+from allure_commons.types import Severity
 
-from selene import be, have
 from selene.support.shared import browser
+from demoqa import practise_form
 
 
-def test_input_name():
-    browser.element('#firstName').should(be.blank).type('Dart')
-    browser.element('#firstName').should(have.value('Dart'))
+@allure.tag('web')
+@allure.severity(Severity.NORMAL)
+@allure.label('owner', 'Vados61')
+@allure.feature('Проверка правильности заполнения формы студента')
+@allure.link('https://github.com', name='Practise-form')
+def test_practis_form():
+    with allure.step('Открываем страницу заполнения формы'):
+        browser.open('https://demoqa.com/automation-practice-form')
 
-    browser.element('#lastName').should(be.blank).type('Weider')
-    browser.element('#lastName').should(have.value('Weider'))
+    with allure.step('Заполняем имя и фамилию'):
+        practise_form.type_name('Dart')
+        practise_form.type_last_name('Weider')
 
+    with allure.step('Заполняем почту'):
+        practise_form.type_email('dartic-martic@starwars.com')
 
-def test_input_email():
-    browser.element('#userEmail').should(be.blank).type('dartic-martic@starwars.com')
-    browser.element('#userEmail').should(have.value('dartic-martic@starwars.com'))
+    with allure.step('Выбираем пол'):
+        practise_form.choose_gender('Male')
 
+    with allure.step('Заполняем телефон'):
+        practise_form.type_phone('8005553555')
 
-def test_input_gender():
-    browser.element('label[for=gender-radio-1]').click()
+    with allure.step('Выставляем дату рождения'):
+        practise_form.choose_date_of_birth(day=15, month='May', year=2100)
 
+    with allure.step('Выбираем предметы'):
+        subjects_list = 'Arts', 'Maths', 'Computer Science'
+        practise_form.choose_sabjects(subjects_list)
 
-def test_input_phone():
-    browser.element('#userNumber').should(be.blank).type('8005553555')
-    browser.element('#userNumber').should(have.value('8005553555'))
+    with allure.step('Выбираем хобби'):
+        practise_form.choose_hobbies(['Reading', 'Music'])
 
+    with allure.step('Выбираем фото из папки media'):
+        practise_form.choose_photo('123.jpeg')
 
-def test_input_date_of_birth():
-    browser.element('#dateOfBirthInput').click()
-    browser.element('.react-datepicker__month-select>option[value="5"]').click()
-    browser.element('.react-datepicker__year-select>option[value="2100"]').click()
-    browser.element('.react-datepicker__day--030:not(.react-datepicker__day--outside-month)').click()
-    browser.element('#dateOfBirthInput').should(have.value('30 Jun 2100'))
+    with allure.step('Заполняем адрес'):
+        practise_form.type_adress('7th planet, 2d star of 234th galaxy')
 
+    with allure.step('Выбираем штат и город'):
+        practise_form.choose_state_and_city(state='NCR', city='Delhi')
 
-def test_subjects_input():
-    subjects_list = 'Arts', 'Maths', 'Computer Science'
-    for subject in subjects_list:
-        browser.element('#subjectsInput').type(subject).press_enter()
-    browser.all('.subjects-auto-complete__value-container .subjects-auto-complete__multi-value__label').should(
-        have.exact_texts(subjects_list))
-
-
-def test_hobbies_check_box():
-    browser.element('label[for=hobbies-checkbox-3]').click()
-
-
-def test_upload_picture():
-    browser.element('#uploadPicture').set_value(
-        os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir, 'media/123.jpeg'))
-    )
-
-
-def test_input_current_address():
-    browser.element('#currentAddress').type('7th planet, 2d star of 234th galaxy')
-    browser.element('#currentAddress').should(have.value('7th planet, 2d star of 234th galaxy'))
-
-
-def test_city_input():
-    browser.element('#react-select-3-input').type('NCR').press_enter()
-    browser.element('#react-select-4-input').type('Delhi').press_enter()
-
-
-def test_submit_button():
-    browser.element('#submit').click()
-
-
-def test_review_results():
-    review_list = [
-        'Student Name Dart Weider',
-        'Student Email dartic-martic@starwars.com',
-        'Gender Male',
-        'Mobile 8005553555',
-        'Date of Birth 30 June,2100',
-        'Subjects Arts, Maths, Computer Science',
-        'Hobbies Music',
-        'Picture 123.jpeg',
-        'Address 7th planet, 2d star of 234th galaxy',
-        'State and City NCR Delhi'
-    ]
-    browser.all('.modal-body tbody tr').should(have.exact_texts(review_list))
-    browser.element('#closeLargeModal').click()
+    with allure.step('Сличаем данные из формы с исходными'):
+        test_data = [
+            'Dart Weider',
+            'dartic-martic@starwars.com',
+            'Male', '8005553555',
+            '15 May,2100',
+            'Arts, Maths, Computer Science',
+            'Reading, Music',
+            '123.jpeg',
+            '7th planet, 2d star of 234th galaxy',
+            'NCR Delhi'
+        ]
+        practise_form.check_submit_data(test_data)
